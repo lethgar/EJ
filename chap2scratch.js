411a0cc2f52e9f687071df48ba63a7d4ed6b0015
lethgar <lethgar@protonmail.com>
2020-12-17 06:27:27 -0500

    Working on triangle problem.

--- chap2scratch.js
+++ chap2scratch.js
@@ -2,12 +2,62 @@
 if (apple <= 25); {
   console.log("hello world");
 }
-*/
-let apollo = 11;
+
+ let apollo = 11;
 let gemini = 13;
-if (apollo <= 12); {
+if (apollo <= 12) {
   console.log("the eagle has landed");
-  else {
+} else {
     console.log("houston we have a problem");
-  }
+  };
+  console.log('done');
+let number = 10;
+while (number <=100) {
+  console.log(number);
+  number = number + 10;
+};
+
+let ent = 1;
+let orc = 10;
+while (ent < orc) {
+  console.log(ent);
+  ent = ent + .5;
+};
+
+
+let result = 1;
+let counter = 0;
+while (counter < 10) {
+    result = result * 2;
+    counter = counter + 1;
+};
+console.log(result);
+
+let result = 1;
+let counter = 0;
+while (counter < 16) {
+  result = result * 3;
+  counter = counter + 1;
+};
+console.log(result);
+
+let result = 1;
+let counter = 0;
+while (counter < 25) {
+    result = result * 5;
+    counter = counter + 1;
+};
+console.log(result);
+
+
+let counter = 0
+let mike = "# ";
+while (counter < 7) {
+  console.log(mike + "\n");
+  counter += 1;
+  mike += "# "
+};
+*/
+for (let counter = 0; counter <= 7; counter += 1){
+  console.log("#" + "\n");
 }
