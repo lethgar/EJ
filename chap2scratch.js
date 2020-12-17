66e7484aaca77f514f74fea047342c3367234975
lethgar <lethgar@protonmail.com>
2020-12-16 21:45:27 -0500

    Else question.

---
+++ chap2scratch.js
@@ -0,0 +1,13 @@
+/* let apple = 25;
+if (apple <= 25); {
+  console.log("hello world");
+}
+*/
+let apollo = 11;
+let gemini = 13;
+if (apollo <= 12); {
+  console.log("the eagle has landed");
+  else {
+    console.log("houston we have a problem");
+  }
+}
