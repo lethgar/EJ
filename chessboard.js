03631553b42db212120e9ba6ae6fcefc741d285e
will <lethgar@protonmail.com>
2020-12-21 11:20:27 -0600

    Chessboard rewrite. This is still too clunky.

--- chessboard.js
+++ chessboard.js
@@ -1,7 +1,8 @@
 const b = '#'
 const w = ' '
-for (var i = 0; i < 8; i++)  {
+for (i = 0; i < 8; i++)  {
   if (i % 2 == 0) {
   console.log(b + w + '\n');
-} else (i)
+} else
   console.log(w + b + '\n');
+};
