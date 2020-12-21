d5243adf853609ebd1cb4090bf6640d8c44d9b75
will <lethgar@protonmail.com>
2020-12-21 12:50:53 -0600

    Add another incorrect attempt, this prints an xmas tree.

--- chessboard.js
+++ chessboard.js
@@ -1,8 +1,23 @@
+/*
 const b = '#'
 const w = ' '
-for (i = 0; i < 8; i++)  {
+for (i = 0; i <= 8; i++)  {
   if (i % 2 == 0) {
   console.log(b + w + '\n');
 } else
   console.log(w + b + '\n');
+};
+*/
+
+let b = '#'
+let w = ' '
+let sizeMax = 8
+for (i = 0; i <= sizeMax ; i++)  {
+  if ((i % 2 == 0)) {
+    console.log(b);
+    b += " #"
+  } if ((i % 2 != 0)) {
+    console.log(w);
+    w += "# "
+  }
 };
