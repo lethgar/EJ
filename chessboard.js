3cf7b419e3e57a3b408cacb901f6f1a6530d5160
lethgar <lethgar@protonmail.com>
2020-12-23 23:39:46 -0500

    Further mistakes in chessboard.

--- chessboard.js
+++ chessboard.js
@@ -1,17 +1,16 @@
-/*
-const b = '#'
-const w = ' '
-for (i = 0; i <= 8; i++)  {
+/* let b = '#'
+let w = ' '
+let sizeMax = 8
+for (i = 0; i <= sizeMax ; i++)  {
   if (i % 2 == 0) {
-  console.log(b + w + '\n');
-} else
-  console.log(w + b + '\n');
-};
-*/
+  console.log(b += " " + '\n');
+} else if (i % 2 != 0)
+  console.log(w += "#" + '\n');
+} */
 
 /* here's the 'working' board. still need to implement user changing sizeMax
  can change overall structure instead of pre-written lines. feels like
- cheating. */
+ cheating.
 let b = ' # # # #'
 let w = '# # # # '
 let sizeMax = 8
@@ -22,3 +21,15 @@ for (i = 0; i <= sizeMax ; i++)  {
     console.log(w);
   }
 };
+*/
+let b = '#';
+let w = ' ';
+let sizeMax = 8;
+for (i = 0; i <= sizeMax ; i++)  {
+  while (i % 2 == 0) {
+    console.log(b);
+  };
+  while (i % 2 != 2) {
+    console.log("kill me");
+  }
+}
