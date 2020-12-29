570d7c9d9123af5b4907e40473b3409772b927b9
lethgar <lethgar@protonmail.com>
2020-12-29 07:19:23 -0500

    Changed code to print un-alternating grid.

--- chessboard.js
+++ chessboard.js
@@ -1,3 +1,5 @@
+/* Final attempt at solo chessboard code
+  12/29/20 */
 let ROWMAX = 8;
 let COLMAX = 8;
 let b = '#';
@@ -5,18 +7,12 @@ let w = '-';
 let stringVal = '';
 
 for (let i = 0; i < ROWMAX; i++) {
-    //console.log("In outer for on pass " + i)
-    for (let j = 0; j < COLMAX ; j ++) {
-    //console.log("In inner for on pass " + j)
-  if (i % 2 == 0) {
-     //console.log("I am in if part of test")
-    stringVal = (w + b);
-     //console.log("Even building stringVal: " + stringVal)
-} else {
-     //console.log("I am in else part of test")
-     stringVal = (b + w);
-     //console.log("Odd building stringVal: " + stringVal)
-   }
+  for (let j = stringVal; j < COLMAX; j ++) {
+    if (j % 2 == 0) {
+     stringVal = (w + stringVal);
+   } else  {
+      stringVal = (b + stringVal);
+    }
   }
   console.log(stringVal);
 };
