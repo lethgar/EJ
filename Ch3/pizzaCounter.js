5053fde18131511bc44cdf3cf8b71b2c982ccf5b
lethgar <lethgar@protonmail.com>
2021-01-08 23:07:24 -0500

    Begin commenting process elements.

--- Ch3/pizzaCounter.js
+++ Ch3/pizzaCounter.js
@@ -1,33 +1,30 @@
+/* Pizza slice counter, a practice vehicle for scope and closure.
+  William O. 1.08.21
+*/
+// create parent function to conrain pizza elements
 function pizza(topping) {
   let slice = 12;
-
+// A piece of information to greet the user
   function greetUser() {
     console.log('Hey look, a pizza!');
   }
-
+// Insert counter to track pizza eating progress
    function eat() {
     for(i=0; i < 12; i++) {
     slice--;
     console.log(`I just ate a piece of ${topping} pizza, ${slice} slices remaining`);
     }
   }
-
+// alert user when out of pizza
   function alert() {
-    if (slice = 0) {
-      console.log(`Oh no, I'm out of ${topping}`);
-    }
+    console.log(`Remaining pizza slices = ${slice}, time to call Dominos!`);
   }
 
-  function descrip() {
-    console.log(`remaining pizza slices = ${slice}, on my ${topping} pizza`);
-  }
-
-  return {eat, greetUser, descrip, alert}
+  return {eat, greetUser, alert}
 };
 
 let cheese = pizza('cheese');
 
 cheese.greetUser(),
   cheese.eat()
-  cheese.descrip()
   cheese.alert();
