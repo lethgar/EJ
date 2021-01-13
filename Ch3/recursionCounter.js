9da9423b7c09061cabb7d3ae546abfc722578243
lethgar <lethgar@protonmail.com>
2021-01-12 22:37:45 -0500

    Fixed counter.

--- Ch3/recursionCounter.js
+++ Ch3/recursionCounter.js
@@ -1,9 +1,14 @@
+/*
+  Print the integers 1-10 in the console. This is my initial test version.
+  */
+// declare parent function counter
 function counter() {
-  let num
+  // initiate variable 'num' to be used in counter
+  let num = 0;
   if (num == 10) {
     return 10;
   } else {
-    for (i = 0; i <=10; i++){
+    for (i = 0; i < 10; i++){
       num += 1;
       console.log(num);
     }
