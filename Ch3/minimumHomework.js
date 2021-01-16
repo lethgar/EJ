f4985cb58cbad1adca163c456bac0fd722aad669
lethgar <lethgar@protonmail.com>
2021-01-16 17:37:47 -0500

    Solved minimum function.

---
+++ Ch3/minimumHomework.js
@@ -0,0 +1,17 @@
+/*
+ funtion 'min' to take two arguments and return minimum
+ Will O. 1.16.21
+ */
+// declare 'min' and provide for 2 parameters (integers)
+function min(x, y){
+  // initial if statement to compare arguments
+  if (x < y){
+    // print value if smaller
+    console.log(x);
+    // backup comparison
+  } else if (y < x){
+    // print value if smaller
+    console.log(y);
+  }
+};
+min(0, -10);
