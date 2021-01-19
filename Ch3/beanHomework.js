232a31f959b153efce4a254b4dd56e7be6161483
lethgar <lethgar@protonmail.com>
2021-01-18 22:52:28 -0500

    Update global HW answers.

---
+++ Ch3/beanHomework.js
@@ -0,0 +1,18 @@
+/*
+  Write  function countBs that takes string as
+  its only argument and returns number that indicates
+  how many uppercase “B” characters in string.
+  Will 1.18.21
+    */
+function countBs(str) {
+  let x,
+   y = 'B';
+  for (i = 0; i < str.length; i++) {
+    if (str[x] = y) {
+    x++;
+  } else {
+  console.log();
+    }
+  }
+}
+countBs('BBC');
--- Ch3/recursionHomework.js
+++ Ch3/recursionHomework.js
@@ -5,11 +5,12 @@
 
 function isEven(x){
   if (x % 2 == 0){
-    return x == x;
-    console.log(isEven());
-} else {
-   console.log(x != x);
-   return x / 2;
-  isEven(x);
+    return 'true';
+  } else if (x % 2 == 1) {
+    return 'false';
+  } else {
+    x -2;
+    isEven(x);
+  }
 }
-console.log(isEven(50));
+console.log(isEven(10));
